765aee8a124ac359fdeee4c9bf2e56ad3a903d5f
ANAMONTEIROMISQUITA <anaclaudia.misquita@gmail.com>
2025-10-27 17:11:04 -0300

    create bombs

--- campo.py
+++ campo.py
@@ -1,1 +1,8 @@
-n
+import random
+
+SIZE = 5        
+BOMBS = 5       
+
+def create_bombs(size, bombs):
+    all_cells = [(r, c) for r in range(size) for c in range(size)]
+    return set(random.sample(all_cells, bombs))
